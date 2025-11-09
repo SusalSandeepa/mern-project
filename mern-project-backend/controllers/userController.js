5ca4b8caad9298adc1ed802a6c043bd941b1abaf
SusalSandeepa <susalsandeepa2002@gmail.com>
2025-11-09 19:19:51 +0530

    updated to latest node version and also change bcrypt package to bcryptjs

--- mern-project-backend/controllers/userController.js
+++ mern-project-backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import User from "../models/user.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
